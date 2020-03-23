bc0f1bdcaa4174bde7af4f172b1ce7279e072aa0
Jacobo Hernandez <jacobolhernandez@gmail.com>
2020-03-23 08:22:43 -0500

    Initial Commit

---
+++ AppStoreClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStoreClone
+//
+//  Created by Jacobo Hernandez on 3/23/20.
+//  Copyright © 2020 Jacobo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
