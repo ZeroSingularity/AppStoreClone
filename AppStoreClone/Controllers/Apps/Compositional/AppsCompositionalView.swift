97fad47343fb54b5ae837454966dd24545036633
Jacobo Hernandez <jacobolhernandez@gmail.com>
2020-04-23 14:26:28 -0500

    Prepped SwiftUI Preview. Added Foundation for Compositional Layout View Controller

---
+++ AppStoreClone/Controllers/Apps/Compositional/AppsCompositionalView.swift
@@ -0,0 +1,60 @@
+//
+//  AppsCompositionalView.swift
+//  AppStoreClone
+//
+//  Created by Jacobo Hernandez on 4/23/20.
+//  Copyright © 2020 Jacobo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+class CompositionalController: UICollectionViewController {
+    fileprivate let cellId = "cellId"
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .systemBackground
+        navigationItem.title = "Apps"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .lightGray
+        return cell
+    }
+}
+
+struct AppsView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let controller = CompositionalController()
+        return UINavigationController(rootViewController: controller)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
+}
+
+struct AppsCompositionalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppsView()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
